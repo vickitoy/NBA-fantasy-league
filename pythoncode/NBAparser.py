a8e57e7519623129453332c6aa9b6ccb0318cf00
ttshimiz <tshimizu@astro.umd.edu>
2015-11-10 12:09:00 -0500

    create function to calculate the remaining wins we each have
    1 win for every game left where we have at least one team playing in

--- pythoncode/NBAparser.py
+++ pythoncode/NBAparser.py
@@ -1,5 +1,8 @@
 import urllib2
 import json
+import pandas as pd
+import datetime as dt
+import numpy as np
 
 def getpage(htmlpage):
     response = urllib2.urlopen(htmlpage)
@@ -35,11 +38,56 @@ def makejson(json_text):
         if any(s in teamname for s in taro):
             twins += teamwins
             tloss += teamloss
+    
+    vwins_remain, jwins_remain, twins_remain = calc_remaining_wins(vicki, johnny, taro)
+    
         #print teamname, teamwins,teamloss
-    print 'Vicki ', vwins, vloss
-    print 'Johnny ', jwins, jloss
-    print 'Taro ', twins, tloss
+    print 'Vicki ', vwins, vloss, vwins_remain
+    print 'Johnny ', jwins, jloss, jwins_remain
+    print 'Taro ', twins, tloss, twins_remain
 
+ 
+def calc_remaining_wins(vicki, johnny, taro):
+
+    # Upload the current schedule
+    nba_sched = pd.read_csv('2015_schedule.txt', index_col=0)
+    
+    # Convert the date to a Timestamp
+    nba_sched.index = pd.to_datetime(nba_sched.index)
+    
+    # Get today's date
+    today = dt.date.today().isoformat()
+    
+    # Remove all of the games that have already been played. Right now assumes today's
+    # games haven't been played yet.
+    nba_sched_remain = nba_sched.loc[nba_sched.index >= today]
+    
+    # Loop through the remaining games. Assume exactly 1 win for every game a person has
+    # a team playing in. If they have 2 teams playing each other, that only counts a 1 win
+    vwins_remain = 0
+    jwins_remain = 0
+    twins_remain = 0
+    
+    for game in np.unique(nba_sched_remain.index):
+    
+        home_teams = nba_sched_remain.loc[game, 'Home/Neutral']
+        away_teams = nba_sched_remain.loc[game, 'Visitor/Neutral']
+        
+        for i in range(len(home_teams)):
+            ht = home_teams[i].split()[1]
+            at = away_teams[i].split()[1]
+            if (any(ht == np.array(vicki)) | any(at == np.array(vicki))):
+                vwins_remain += 1
+        
+            if (any(ht == np.array(johnny)) | any(at == np.array(johnny))):
+                jwins_remain += 1
+
+            if (any(ht == np.array(taro)) | any(at == np.array(taro))):
+                twins_remain += 1
+    
+    return vwins_remain, jwins_remain, twins_remain
+        
+    
     
 def parser():
     website = 'http://stats.nba.com/stats/leaguedashteamstats?Season=2015' +\
