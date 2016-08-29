e1c81328319225cab78526e135216d8f3090729b
ttshimiz <tshimizu@astro.umd.edu>
2016-08-28 22:33:15 -0400

    clean up

--- pythoncode/NBAparser.py
+++ pythoncode/NBAparser.py
@@ -25,67 +25,6 @@ def main():
             taro_ids.append(tm['id'])
         else:
             raise ValueError('{0} not on any team!'.format(tm['name']))
-        
-    
-#    vicki_orig  = ['Spurs', 'Rockets', 'Grizzlies', 'Heat', 'Wizards', 
-#              'Bucks', 'Mavericks', 'Magic', 'Nets', 'Knicks']
-#    johnny_orig = ['Warriors', 'Thunder', 'Celtics', 'Pelicans', 'Bulls',
-#              'Jazz', 'Suns', 'Timberwolves', 'Nuggets', 'Lakers']
-#    taro_orig   = ['Cavaliers', 'Clippers', 'Hawks', 'Raptors', 'Hornets',
-#             'Pacers', 'Pistons', 'Kings', 'Blazers', '76ers']
-              
-
-#    tradedate = 'March 16'
-    
-    # Thunder for Nets
-#    vicki_trade1  = ['Spurs', 'Rockets', 'Grizzlies', 'Heat', 'Wizards', 
-#              'Bucks', 'Mavericks', 'Magic', 'Thunder', 'Knicks']
-#    johnny_trade1 = ['Warriors', 'Nets', 'Celtics', 'Pelicans', 'Bulls',
-#              'Jazz', 'Suns', 'Timberwolves', 'Nuggets', 'Lakers']
-#    taro_trade1   = ['Cavaliers', 'Clippers', 'Hawks', 'Raptors', 'Hornets',
-#             'Pacers', 'Pistons', 'Kings', 'Blazers', '76ers']    
-
-
-
-#    if alt == True:
-#        vicki  = ['Spurs', 'Clippers', 'Hawks', 'Heat', 'Wizards', 
-#                'Rockets', 'Nuggets', 'Pelicans', '76ers', 'Knicks']
-#        johnny = ['Warriors', 'Raptors', 'Celtics', 'Grizzlies', 'Bulls',
-#                'Jazz', 'Pacers', 'Timberwolves', 'Kings', 'Lakers']
-#        taro   = ['Cavaliers', 'Thunder', 'Hornets', 'Mavericks', 'Blazers',
-#                'Magic', 'Pistons', 'Bucks', 'Suns', 'Nets']
-    
-#    if aftertrade == True:
-#        vicki  = vicki_trade1
-#        johnny = johnny_trade1
-#        taro   = taro_trade1
-        
-#    if aftertrade == False:
-#        vicki  = vicki_orig
-#        johnny = johnny_orig
-#        taro   = taro_orig
-        
-#    print vicki
-               
-#    data = json.loads(json_text)
-    
-#    vwins = 0; vloss = 0
-#    jwins = 0; jloss = 0
-#    twins = 0; tloss = 0
-#    for teamdata in data['resultSets'][0]['rowSet']:
-#        teamname = teamdata[1]
-#        teamwins = teamdata[3]
-#        teamloss = teamdata[4]
-        
-#        if any(s in teamname for s in vicki):
-#            vwins += teamwins
-#            vloss += teamloss
-#        if any(s in teamname for s in johnny):
-#            jwins += teamwins
-#            jloss += teamloss
-#        if any(s in teamname for s in taro):
-#            twins += teamwins
-#            tloss += teamloss
 
     vrecord, jrecord, trecord = calc_current_wins(vicki_ids, johnny_ids, taro_ids)
     vwins = vrecord[0]; vloss = vrecord[1]
@@ -93,11 +32,6 @@ def main():
     twins = trecord[0]; tloss = trecord[1]
     
     vwins_remain, jwins_remain, twins_remain = calc_remaining_wins(vicki, johnny, taro)
-    
-        #print teamname, teamwins,teamloss
-    #print 'Vicki ', vwins, vloss, vwins_remain
-    #print 'Johnny ', jwins, jloss, jwins_remain
-    #print 'Taro ', twins, tloss, twins_remain
 
     return [[vwins,vloss,vwins_remain], 
             [jwins,jloss,jwins_remain],
@@ -178,52 +112,6 @@ def calc_remaining_wins(vicki, johnny, taro):
                     twins_not_guaranteed += 1
     
     return vwins_remain, jwins_remain, twins_remain
-        
-    
-    
-#def parser(alt=False):
-    #website = 'http://stats.nba.com/stats/leaguedashteamstats?Season=2015' +\
-    #            '-16&AllStarSeason&SeasonType=Regular%20Season&LeagueID=00&' +\
-    #            'MeasureType=Base&PerMode=PerGame&PlusMinus=N&PaceAdjust=N&' +\
-    #            'Rank=N&Outcome&Location&Month=0&SeasonSegment&DateFrom&Date' +\
-    #            'To&OpponentTeamID=0&VsConference&VsDivision&GameSegment&' +\
-    #            'Period=0&LastNGames=0&GameScope&PlayerExperience&Player' +\
-    #            'Position&StarterBench'
-
-    # Before trade date
-#    website_bt = 'http://stats.nba.com/stats/leaguedashteamstats?Season=2015' +\
-#                '-16&AllStarSeason&SeasonType=Regular%20Season&LeagueID=00&' +\
-#                'MeasureType=Base&PerMode=PerGame&PlusMinus=N&PaceAdjust=N&' +\
-#                'Rank=N&Outcome&Location&Month=0&SeasonSegment&DateFrom&Date' +\
-#                'To=March15&OpponentTeamID=0&VsConference&VsDivision&GameSegment&' +\
-#                'Period=0&LastNGames=0&GameScope&PlayerExperience&Player' +\
-#                'Position&StarterBench'
-                                
-    #print website
-#    json_text = getpage(website_bt)
-#    [[vwins_bt,vloss_bt,vwins_remain_bt], 
-#     [jwins_bt,jloss_bt,jwins_remain_bt],
-#     [twins_bt,tloss_bt,twins_remain_bt]] = makejson(json_text, alt=alt, aftertrade=False)
-    
-    # After trade date
-#    website_at = 'http://stats.nba.com/stats/leaguedashteamstats?Season=2015' +\
-#                '-16&AllStarSeason&SeasonType=Regular%20Season&LeagueID=00&' +\
-#                'MeasureType=Base&PerMode=PerGame&PlusMinus=N&PaceAdjust=N&' +\
-#                'Rank=N&Outcome&Location&Month=0&SeasonSegment&DateFrom=March16&Date' +\
-#                'To&OpponentTeamID=0&VsConference&VsDivision&GameSegment&' +\
-#                'Period=0&LastNGames=0&GameScope&PlayerExperience&Player' +\
-#                'Position&StarterBench'
-                
-    #print website
-#    json_text = getpage(website_at)
-#    [[vwins_at,vloss_at,vwins_remain_at], 
-#     [jwins_at,jloss_at,jwins_remain_at],
-#     [twins_at,tloss_at,twins_remain_at]]  = makejson(json_text, alt=alt, aftertrade=True)
-
-    
-#    print 'Vicki ', vwins_bt+vwins_at, vloss_bt+vloss_at, vwins_remain_at
-#    print 'Johnny ', jwins_bt+jwins_at, jloss_bt+jloss_at, jwins_remain_at
-#    print 'Taro ', twins_bt+twins_at, tloss_bt+tloss_at, twins_remain_at
     
     
     
