d59963f7976f493cfbb4ed18b1fb54453d36e48b
Vicki Toy <vickitoy@gmail.com>
2018-05-11 07:54:20 -0400

    Replace NBAparser2 with object oriented script
    - Use bet_calculator in conjunction with new Bettor and Team classes
    - Remove old or unnecessary code
    - Ignore many files that get overwritten every run

---
+++ pythoncode/bet_calculator.py
@@ -0,0 +1,187 @@
+#!/bin/python
+
+import pandas as pd
+import datetime as dt
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import seaborn as sn
+import time
+import pytz
+import os
+
+import config
+from bettor import Bettor, Team
+
+bet_file_postfix = config.BET_FILE_POSTFIX
+schedule_file = config.SCHEDULE_FILE
+SEASON = config.SEASON
+START_DATE = config.START_DATE
+tz = pytz.timezone('EST')
+today = dt.datetime.now(tz)
+STR_TODAY = '%i/%i/%i' % (today.month, today.day, today.year)
+
+
+def todays_games(person_list):
+    team_bettor_dict = {team: person_obj.name for person_obj in person_list for team in person_obj.team_objs.keys()}
+    team_bettor_dict['Blazers'] = team_bettor_dict['Trail Blazers']
+    team_bettor_dict['76ers'] = team_bettor_dict['Sixers']
+    # Upload the current schedule and convert date to timestamp
+    nba_sched = pd.read_csv(schedule_file, index_col=0)
+    nba_sched.index = pd.to_datetime(nba_sched.index)
+
+    # Get today's date
+    today = dt.datetime.strptime(STR_TODAY, '%m/%d/%Y').isoformat()
+
+    # Grabs todays games
+    nba_sched_today = nba_sched.loc[nba_sched.index == today]
+    nba_sched_today['HomeShort'] = nba_sched_today['Home/Neutral'].str.split().str[-1]
+    nba_sched_today['VisitorShort'] = nba_sched_today['Visitor/Neutral'].str.split().str[-1]
+
+    games = ''
+    for index, row in nba_sched_today.iterrows():
+        ht = row['HomeShort']
+        at = row['VisitorShort']
+
+        games += html_table_add_row([ht + ' ('+team_bettor_dict[ht].title()+')',at + ' ('+team_bettor_dict[at].title()+')'])
+
+    return games
+
+
+def plot_graph_all(person_list):
+    """Creates bettor 30-day and all season Wins-Losses graphs and creates
+       graphs that displays the Wins-Losses for a bettor's set of teams"""
+
+    # Create a DF with every calendar day with the summed Wins-Losses for each bettor
+    summed_diff_calendar_list = [reduce((lambda x, y: x+y),
+        [person_obj.team_objs[team].diff_calendar() for team in person_obj.teams])
+        for person_obj in person_list ]
+
+     # Plot Wins-Losses as a function of time for last 30 days
+    sn.set(color_codes=True)
+    fig = plt.figure(figsize=(6.5, 6.5))
+    ax = fig.add_subplot(111)
+    for i in range(len(person_list)):
+        summed_diff_calendar_list[i][today - dt.timedelta(days=30):today].plot()
+    ax.legend(pick_order, loc='lower left')
+    ax.set_ylabel('Wins - Losses', fontsize=12)
+    fig.savefig('../images/win_percent_recent.png', bbox_inches='tight')
+    plt.close(fig)
+
+     # Plot Wins-Losses as a function of time for season so far
+    sn.set(color_codes=True)
+    fig = plt.figure(figsize=(6.5, 3))
+    ax = fig.add_subplot(111)
+    for i in range(len(person_list)):
+        summed_diff_calendar_list[i].plot()
+    ax.legend([person_obj.name for person_obj in person_list], loc='lower left')
+    ax.set_ylabel('Wins - Losses', fontsize=12)
+    fig.savefig('../images/win_percent_all.png', bbox_inches='tight')
+    plt.close(fig)
+
+    # Plot Wins-Losses for each bettor by team
+    sn.set_palette(sn.color_palette("hls", 10))
+    for order, person_obj in enumerate(person_list):
+        # DF with every calendar day for each of the Bettor's teams
+        team_diff_calendar_list = [person_obj.team_objs[team].diff_calendar() for team in person_obj.teams]
+        fig = plt.figure(figsize=(10, 6.5))
+        ax = fig.add_subplot(111)
+        for iteam in person_obj.teams:
+            person_obj.team_objs[iteam].diff_calendar().plot(label=iteam, ax=ax)
+        ax.set_ylabel('Wins - Losses', fontsize=12)
+        ax.legend()
+        fig.savefig('../images/'+str(order+1)+'_teams.png', bbox_inches='tight')
+        plt.close(fig)
+
+    return
+
+def make_html(person_list):
+    """Populate main HTML page that summarizes bettors status and tonight's matchups"""
+    with open('../template.html', 'r') as ftemp:
+        temp = ftemp.read()
+
+    # Person with the most wins
+    current_winner = max([(person_obj.all_wins(), person_obj.name) for person_obj in person_list])[1]
+
+    # Use html_string to dump information into HTML page
+    html_string = [current_winner, STR_TODAY, current_winner.title()]
+
+    # Every bettor's quick stats
+    wins, losses, remaining = [],[],[]
+    for person_obj in person_list:
+        html_string += [person_obj.name.title()]
+        wins.append(person_obj.all_wins())
+        losses.append(person_obj.all_losses())
+        remaining.append(person_obj.all_remaining()-person_obj.dup_remaining_games())
+
+    html_string += wins + losses + remaining
+
+    # Order of teams that were picked by a bettor
+    # -> Bettor 1 pick 1, Bettor 2 pick 1, Bettor 3 pick 1, Bettor 1 pick 2, etc
+    team_pickorder = [person_obj.name.title() for person_obj in person_list] + [person_obj.teams[i] for i in range(10) for person_obj in person_list]
+
+    html_string += team_pickorder
+
+    # Today's games (by team and bettor)
+    html_string += [todays_games(person_list)]
+    print html_string
+    updated = temp % tuple(html_string)
+
+    with open('../bet.html', 'w') as findex:
+        findex.write(updated)
+
+    return
+
+def make_bettor_html(person_list):
+    """Make individual bettor page information that breaks down a bettor's teams"""
+    with open('../template_bettor.html', 'r') as ftemp:
+        temp = ftemp.read()
+    html_string = [person_obj.name.title() for person_obj in person_list]
+    for person_obj in person_list:
+
+        team_list = ''
+        # Summary of team information for a bettor
+        for teamname, team_obj in person_obj.team_objs.iteritems():
+            team_list += html_table_add_row([teamname,
+                                             team_obj.wins(),
+                                             team_obj.losses(),
+                                             team_obj.remaining(),
+                                             team_obj.last10()], align="center")
+
+        html_string += [person_obj.name.title(), person_obj.all_wins(), person_obj.all_losses(), person_obj.all_remaining()-person_obj.dup_remaining_games(),team_list]
+
+    updated = temp % tuple(html_string)
+
+    with open('../bettors.html', 'w') as findex:
+        findex.write(updated)
+
+    return
+
+def html_table_add_row(element_list, align="left"):
+    """Simple html table row"""
+    connector = '    </td><td align="'+align+'">'
+    table_str = '  <tr><td align="'+align+'">'
+    table_str += connector.join(str(e) for e in element_list)
+    table_str += '  </td></tr>'
+    return table_str
+
+if __name__ == '__main__':
+
+    # Read in bettor pick order
+    pick_order = np.loadtxt('pickorder.txt', dtype=str, delimiter='\t')
+
+    # Create list of Bettor objects and initialize them
+    person_list = []
+    for order, bettor in enumerate(pick_order):
+        bettor = bettor.lower()
+        bettor_teams = np.loadtxt(bettor + bet_file_postfix, dtype=str, delimiter='\t')
+
+        bettor_obj = Bettor(bettor, order, bettor_teams)
+
+        person_list += [bettor_obj]
+
+    # Create graphs and html pages
+    plot_graph_all(person_list)
+    make_html(person_list)
+    make_bettor_html(person_list)
---
+++ pythoncode/bettor.py
@@ -0,0 +1,123 @@
+from nba_py import team
+import pytz
+import datetime as dt
+import pandas as pd
+import time
+
+import config
+
+schedule_file = config.SCHEDULE_FILE
+SEASON = config.SEASON
+START_DATE = config.START_DATE
+tz = pytz.timezone('EST')
+today = dt.datetime.now(tz)
+STR_TODAY = '%i/%i/%i' % (today.month, today.day, today.year)
+END_DATE = '%i-%i-%i' % (today.year, today.month, today.day)
+idx = pd.date_range(START_DATE, END_DATE)
+
+# Upload the current schedule and convert date to timestamp
+nba_sched = pd.read_csv(schedule_file, index_col=0)
+nba_sched.index = pd.to_datetime(nba_sched.index)
+teamids = {value['name']:{'id': value['id'],'abbr':value['abbr']} for key,value in team.TEAMS.iteritems()}
+
+class Bettor():
+    """Class for a bettor or person that has an array of Team objects and other
+       useful utilities"""
+    def __init__(self, name, order, teams):
+        print name
+        self.name = name
+        self.order = order
+        self.teams = teams
+        self.team_objs = self.all_teams()
+
+    # Creates dictionary of Team objects where the key is the teamname
+    def all_teams(self):
+        team_obj_dict = {}
+        for teamname in self.teams:
+            print teamname
+            team_obj_dict[teamname] = Team(teamname)
+            time.sleep(0.5)
+        return team_obj_dict
+
+    # Calculate how many games left in a season that a Bettor plays their own team (int)
+    def dup_remaining_games(self):
+        # Get today's date
+        today = dt.datetime.strptime(STR_TODAY, '%m/%d/%Y').isoformat()
+
+        # Remove all of the games that have already been played. Assumes today's
+        # games haven't been played yet.
+        nba_sched_remain = nba_sched.loc[nba_sched.index >= today]
+        nba_sched_remain['Home'] = nba_sched_remain['Home/Neutral'].str.split().str[-1]
+        nba_sched_remain['Visitor'] = nba_sched_remain['Visitor/Neutral'].str.split().str[-1]
+        bettor_teams = list(self.teams)
+        if 'Trail Blazers' in bettor_teams:
+            bettor_teams.append('Blazers')
+        if 'Sixers' in bettor_teams:
+            bettor_teams.append('76ers')
+        dups_games = nba_sched_remain[(nba_sched_remain['Home'].isin(bettor_teams)) &
+                                      (nba_sched_remain['Visitor'].isin(bettor_teams)) ]
+        return dups_games.shape[0]
+
+    # Returns all current wins (int)
+    def all_wins(self):
+        return sum([self.team_objs[team].wins() for team in self.team_objs])
+
+    # Returns all current losses (int)
+    def all_losses(self):
+        return sum([self.team_objs[team].losses() for team in self.team_objs])
+
+    # Returns all remaining games (int)
+    def all_remaining(self):
+        return len(self.teams)*82 - self.all_wins() - self.all_losses()
+
+class Team():
+    """Class for a team that contains gamelog information and other
+       useful utilities"""
+    def __init__(self, teamname):
+        self.name = teamname
+        self.id = teamids[self.name]['id']
+        self.abbr = teamids[self.name]['abbr']
+        self.gamelog = self.gamelog_proc()
+
+    # Processes gamelog with correct parameters
+    def gamelog_proc(self):
+        gamelog = team.TeamGameLogs(self.id, season=SEASON).info()[['GAME_DATE', 'WL']]
+        gamelog['GAME_DATE'] = pd.to_datetime(gamelog['GAME_DATE'])
+        return gamelog
+
+    # Calculate the last 10 games W-L (str)
+    def last10(self):
+        last10gameswins = sum(self.gamelog['WL'][:10].str.count('W'))
+        return '%i-%i' % (last10gameswins, max(10-last10gameswins, 0))
+
+    # The populated full calendar DF that fills in missing dates with a dash
+    def full_calendar_gamelog(self):
+        df = self.gamelog.set_index(['GAME_DATE']).reindex(idx, fill_value='-').truncate(after=STR_TODAY)
+        df.fillna(value='-', inplace=True)
+        return df
+
+    # The running sum of wins for full calendar (DF)
+    def wins_calendar(self):
+        df = self.full_calendar_gamelog()
+        return df['WL'].cumsum().str.count('W')
+
+    # The running sum of losses for full calendar (DF)
+    def losses_calendar(self):
+        df = self.full_calendar_gamelog()
+        return df['WL'].cumsum().str.count('L')
+
+    # The running sum of wins-losses for full calendar (DF)
+    def diff_calendar(self):
+        return self.wins_calendar() - self.losses_calendar()
+
+    # Team's current wins (int)
+    def wins(self):
+        return self.wins_calendar()[-1]
+
+    # Team's current losses (int)
+    def losses(self):
+        return self.losses_calendar()[-1]
+
+    # Team's number of remaining games in the season (int)
+    def remaining(self):
+        return 82-self.wins() - self.losses()
